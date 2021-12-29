76f7899021ea62cbdaff376624538c2f1880f169
Fadela Numah Kadenz <75234524+FadelYang@users.noreply.github.com>
2021-12-29 14:41:27 +0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -10,4 +10,4 @@ hasilK = ((C*5)/5)+273
 
 print(C,"derajat Celcius", '=', hasilF, "deraja Fahrenheit")
 print(C,"derajat Celcius", '=', hasilR, "derajat Reamur ")
-print(C,"derajat Celcius", '=', hasilK, "deraja Kelvin")
+print(C,"derajat Celcius", '=', hasilK, "derajat Kelvin")
